80682daf7927dd316b9bd46d3e6a0ff8653c10cf
Zeel Modi <zeel91297@gmail.com>
2019-08-20 19:22:30 +0530

    for deployment

--- dbconnection.js
+++ dbconnection.js
@@ -1,9 +1,16 @@
 var mysql = require('mysql');
 
-var connection = mysql.createPool({
+/* var connection = mysql.createPool({
     user: 'root',
     password: '',
     database: 'labour_log'
+}); */
+
+var connection = mysql.createPool({
+    host: 'labourlogdb.mysql.database.azure.com',
+    user: 'labour@labourlogdb',
+    password: 'Workforce@2019',
+    database: 'public'
 });
 
 module.exports = connection;
