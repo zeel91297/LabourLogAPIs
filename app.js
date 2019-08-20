53511cf7bda3997b24eafa7823fb4932cb926950
Zeel Modi <zeel91297@gmail.com>
2019-08-20 19:41:45 +0530

    changes

--- app.js
+++ app.js
@@ -65,9 +65,9 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  //res.status(err.status || 500);
-  console.log(err);
-  res.status(err.status);
+  res.status(err.status || 500);
+  //console.log(err);
+
   res.render('error');
 });
 
