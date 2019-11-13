9b5d2c136ff65660c70bd18f3225b2ca2d6e6a2e
Zeel Modi <zeel91297@gmail.com>
2019-11-13 19:55:55 +0530

    remote server

--- dbconnection.js
+++ dbconnection.js
@@ -6,11 +6,19 @@ var mysql = require('mysql');
     database: 'labour_log'
 }); */
 
-var connection = mysql.createPool({
+/* var connection = mysql.createPool({
     host: 'labourlogdb.mysql.database.azure.com',
     user: 'labour@labourlogdb',
     password: 'Workforce@2019',
     database: 'public'
+}); */
+
+var connection = mysql.createPool({
+    host: 'remotemysql.com',
+    user: 'Mf4QbWGM42',
+    password: 'dF0bcHUntf',
+    database: 'Mf4QbWGM42',
+    port: 3306
 });
 
 module.exports = connection;
